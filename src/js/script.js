1dc0aedbcb3265b71f5c50c56d68a762540a51e3
Kitaeva <KitaevaNasti@yandex.ru>
2025-05-26 01:18:24 +0500

    added shop page

--- src/js/script.js
+++ src/js/script.js
@@ -1,59 +1,74 @@
 $(document).ready(function(){
-    $('.top-slider__inner').slick({
-        dots: true,
-        arrows: false,
-        fade: true,
-        autoplay: true,
-        autoplaySpeed: 2000
-    });
+  $('.shop-content__filter-btn').on('click', function() {
+    $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
+    $(this).addClass('shop-content__filter-btn--active');
+  });
 
-    $(".star").rateYo({
-        starWidth: "17px",
-        normalFill: "#ccccce",
-        ratedFill: "#ffc35b",
-        readOnly: true
-    });
+  $('.button-list').on('click', function(){
+    $('.product-item').addClass('product-item--list');
+  });
 
-    function getTimeRemaining(endtime) {
-      const total = Date.parse(endtime) - Date.parse(new Date());
-      const seconds = Math.floor((total / 1000) % 60);
-      const minutes = Math.floor((total / 1000 / 60) % 60);
-      const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-      const days = Math.floor(total / (1000 * 60 * 60 * 24));
-      
-      return {
-        total,
-        days,
-        hours,
-        minutes,
-        seconds
-      };
-    }
+  $('.button-grid').on('click', function(){
+    $('.product-item').removeClass('product-item--list');
+  });
 
+
+  $('.top-slider__inner').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+  
+  $('.star').rateYo({
+    starWidth: "17px",
+    normalFill: "#ccccce",
+    ratedFill: "#ffc35b",
+    readOnly: true
+  });
+  
+  function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    
+    return {
+      total,
+      days,
+      hours,
+      minutes,
+      seconds
+    };
+  }
+  
     function initializeClock(selector, endtime) {
       const clock = document.querySelector('.promo__clock');
       const daysSpan = clock.querySelector('.days');
       const hoursSpan = clock.querySelector('.hours');
       const minutesSpan = clock.querySelector('.minutes');
       const secondsSpan = clock.querySelector('.seconds');
-
+      
       function updateClock() {
         const t = getTimeRemaining(endtime);
-
+        
         daysSpan.innerHTML = t.days;
         hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
         secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
+        
         if (t.total <= 0) {
           clearInterval(timeinterval);
         }
       }
-
+      
       updateClock();
       const timeinterval = setInterval(updateClock, 1000);
     }
-
+    
     const deadline = $('.promo__clock').attr('data-time');
     initializeClock('ppromo__clock', deadline);
+    
   });
